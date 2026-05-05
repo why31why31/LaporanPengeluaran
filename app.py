6e114451d9519537faef324f15e2464df869a40e
why31why31 <why31why31@gmail.com>
2026-05-05 14:54:24 +0700

    app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def upload_to_drive(file_path, file_name):
     
     # Metadata file
     file_metadata = {
-        'name': Laporann Pengeluaran,
+        'name': Laporann_Pengeluaran,
         'parents': [FOLDER_ID_DRIVE]
     }
     
