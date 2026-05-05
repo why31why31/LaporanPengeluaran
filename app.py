c87ef5a56bb0111842747955d6bf7e239b6a5419
why31why31 <why31why31@gmail.com>
2026-05-05 13:42:25 +0700

    app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import os
 
 # --- KONFIGURASI GOOGLE DRIVE ---
 # Folder ID didapat dari URL folder Google Drive Anda
-FOLDER_ID_DRIVE = "https://drive.google.com/drive/folders/1jlY7VVmTHqVgX8q7yX7YWEvW4ehE3Lqm?usp=drive_link"
+FOLDER_ID_DRIVE = "https://drive.google.com/drive/folders/1ITsQrx3hQe6XxSWs_j7G8t7pmFKdwZoX?usp=drive_link"
 
 def get_drive_service():
     # Mengambil kunci rahasia dari Streamlit Secrets (aman untuk GitHub)
